93630383b2cc8d85a3e744291d116782aee0a7df
bandi72006 <bandi72006@gmail.com>
2021-10-11 18:54:12 +0400

    Fixed clustering issue where species would overlap and wouldn't show

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 #Bandar Al Aish
 
 import pygame
-
+import random
 from species import * 
 
 pygame.init()  
 screen = pygame.display.set_mode((1280,720))  
 run = True
 
-target = [500,500]
+target = [random.randint(0,1280), random.randint(0,720)]
 
-leader = Species(1001,1001, target, (0,0,255))
-species = [Species(100*i, 600, [leader.x, leader.y], (255,0,0)) for i in range(10)]
+leader = Species(random.randint(0,1280), random.randint(0,720), target, (0,0,255))
+species = [Species(random.randint(0,1280), random.randint(0,720), [leader.x, leader.y], (255,0,0)) for i in range(10)]
+
+frames = 0
 
 while True:  
     screen.fill((0,200,0))
+    
+    frames += 1
     pygame.draw.circle(screen, (255,255,0), (target[0], target[1]), 7)
 
+    if frames > 2000:
+        target = [random.randint(0,1280), random.randint(0,720)]
+        frames = 0
+
     for i in species:
-        i.move(leader)
+        i.move(leader.x, leader.y)
         i.draw(screen)
     
-    leader.move(leader)
+    leader.move(target[0], target[1])
     leader.draw(screen)
 
     for event in pygame.event.get():  
--- species.py
+++ species.py
@@ -2,6 +2,7 @@
 
 import pygame
 import math
+import random
 
 class Species:
     def __init__(self, x, y, target, colour):
@@ -10,19 +11,17 @@ class Species:
         self.target = target
         self.colour = colour
 
-    def move(self, leader):
-        if leader.x != self.x:
-            if leader.y != self.y:
-                self.target = [leader.x, leader.y]
+    def move(self, xTar, yTar):
 
-        self.rotation = 10*(math.degrees(math.atan((self.target[1]-self.y)/(self.target[0]-self.x))))
+        self.target = [xTar, yTar]
+
+        self.rotation = 20*(math.degrees(math.atan((self.target[1]-self.y)/(self.target[0]-self.x))))
         changeInX = math.cos(math.radians(self.rotation)) 
         changeInY = math.sin(math.radians(self.rotation))
 
-        self.x += changeInX*0.05
-        self.y += changeInY*0.05
-        if self.colour == (0,0,255):
-            print(self.rotation)
+        self.x += changeInX*random.random()
+        self.y += changeInY*random.random()
+        print(random.random())
 
         if self.x > 1280:
             self.x = 1280
